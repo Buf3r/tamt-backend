868d1bd2988ec2e76437d7f7eca560515df4782d
Carlos Daniel Morillo <xmorillocdx@gmail.com>
2026-03-12 18:46:40 -0400

    Fix PORT variable expansion

--- start.sh
+++ start.sh
@@ -2,4 +2,10 @@
 
 printf "CI_ENVIRONMENT = production\napp.baseURL = https://dbsubasta-production.up.railway.app/\ndatabase.default.hostname = switchyard.proxy.rlwy.net\ndatabase.default.database = railway\ndatabase.default.username = root\ndatabase.default.password = kphYyKHHiTBjQDacodwZONPuwpZipLPs\ndatabase.default.port = 43411\ndatabase.default.DBDriver = MySQLi\n" > /var/www/html/.env
 
-php spark serve --host 0.0.0.0 --port $PORT
+echo "Starting on port: $PORT"
+php spark serve --host 0.0.0.0 --port ${PORT:-8080}
+```
+
+Y en Railway → db_subasta → **Settings → Start Command** asegúrate que dice exactamente:
+```
+/start.sh
