7ea60ccc90e43d69fa06d042bc5491c53da28d45
Carlos Daniel Morillo <xmorillocdx@gmail.com>
2026-03-12 01:06:12 -0400

    Fix bash variable names with dots

--- start.sh
+++ start.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# Escribir variables al .env de CodeIgniter
 cat > /var/www/html/.env << EOF
 CI_ENVIRONMENT = ${CI_ENVIRONMENT}
-app.baseURL = ${app.baseURL}
-database.default.hostname = ${database.default.hostname}
-database.default.database = ${database.default.database}
-database.default.username = ${database.default.username}
-database.default.password = ${database.default.password}
-database.default.port = ${database.default.port}
+app.baseURL = ${app_baseURL}
+database.default.hostname = ${database_default_hostname}
+database.default.database = ${database_default_database}
+database.default.username = ${database_default_username}
+database.default.password = ${database_default_password}
+database.default.port = ${database_default_port}
 database.default.DBDriver = MySQLi
 EOF
-
-# Iniciar Apache
-apache2-foreground
