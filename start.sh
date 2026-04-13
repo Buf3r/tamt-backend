3400c217444c6502a3d1f538c7b3be66088b4833
Carlos Daniel Morillo <xmorillocdx@gmail.com>
2026-04-13 00:11:37 -0400

    Primer commit: Estructura inicial de Alquila tu tiempo 2

--- start.sh
+++ start.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
-# Escribir variables normales al .env
-printf "CI_ENVIRONMENT = production\napp.baseURL = https://dbsubasta-production.up.railway.app/\ndatabase.default.hostname = switchyard.proxy.rlwy.net\ndatabase.default.database = railway\ndatabase.default.username = root\ndatabase.default.password = kphYyKHHiTBjQDacodwZONPuwpZipLPs\ndatabase.default.port = 43411\ndatabase.default.DBDriver = MySQLi\nJWT_SECRET_KEY = ${JWT_SECRET_KEY}\nJWT_TTL = ${JWT_TTL}\nCLOUDINARY_CLOUD_NAME = ${CLOUDINARY_CLOUD_NAME}\nCLOUDINARY_API_KEY = ${CLOUDINARY_API_KEY}\nCLOUDINARY_API_SECRET = ${CLOUDINARY_API_SECRET}\n" > /var/www/html/.env
+# Escribir variables al .env de CI4
+printf "CI_ENVIRONMENT = production\n\
+app.baseURL = ${APP_BASE_URL}\n\
+database.default.hostname = ${DB_HOSTNAME}\n\
+database.default.database = ${DB_DATABASE}\n\
+database.default.username = ${DB_USERNAME}\n\
+database.default.password = ${DB_PASSWORD}\n\
+database.default.port = ${DB_PORT}\n\
+database.default.DBDriver = MySQLi\n\
+JWT_SECRET_KEY = ${JWT_SECRET_KEY}\n\
+JWT_TTL = ${JWT_TTL}\n\
+CLOUDINARY_CLOUD_NAME = ${CLOUDINARY_CLOUD_NAME}\n\
+CLOUDINARY_API_KEY = ${CLOUDINARY_API_KEY}\n\
+CLOUDINARY_API_SECRET = ${CLOUDINARY_API_SECRET}\n" > /var/www/html/.env
 
-# Agregar FCM_CREDENTIALS por separado para evitar problemas con caracteres especiales
 echo "FCM_CREDENTIALS = ${FCM_CREDENTIALS}" >> /var/www/html/.env
-
 echo "ADMIN_KEY = ${ADMIN_KEY}" >> /var/www/html/.env
+echo "GOOGLE_CLIENT_ID = ${GOOGLE_CLIENT_ID}" >> /var/www/html/.env
+echo "FACEBOOK_APP_ID = ${FACEBOOK_APP_ID}" >> /var/www/html/.env
+echo "FACEBOOK_APP_SECRET = ${FACEBOOK_APP_SECRET}" >> /var/www/html/.env
 
 # Correr migraciones si se pasa el argumento
 if [ "$1" = "migrate" ]; then
